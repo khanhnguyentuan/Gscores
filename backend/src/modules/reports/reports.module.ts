b39538ff3cdf6a5d6f515a727c0903dd0e2312d3
khanhnguyentuan <khanh.nguyentuan123@hcmut.edu.vn>
2025-05-20 00:42:23 +0700

    Update for docker

--- backend/src/modules/reports/reports.module.ts
+++ backend/src/modules/reports/reports.module.ts
@@ -8,13 +8,15 @@ import { Subject } from '../subjects/entities/subject.entity';
 import { ScoreLevelBySubject } from './entities/score-level-by-subject.entity';
 import { TopStudentsGroupA } from './entities/top-students-group-a.entity';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Student, Score, Subject, ScoreLevelBySubject, TopStudentsGroupA]),
     CacheModule.register({
       ttl: 3600, // thời gian cache (giây)
-    })
+    }),
+    ScheduleModule.forRoot()
   ],
   controllers: [ReportsController],
   providers: [ReportsService],
