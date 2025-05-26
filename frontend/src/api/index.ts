99a23b56fea15833a2058b431dc037659fbd0e5b
khanhnguyentuan <khanh.nguyentuan123@hcmut.edu.vn>
2025-05-26 18:57:19 +0700

    Fix use web api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_BASE_URL } from '../utils/api';
 
 // Sử dụng API_BASE_URL từ utils/api thay vì hardcode URL
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://backendgscores-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
